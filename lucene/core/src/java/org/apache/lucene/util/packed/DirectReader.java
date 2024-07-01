8a7d070f909907f5edf5efb6b4db05155eadecd4
Armin Braun <me@obrown.io>
2024-07-01 10:42:09 +0200

    faster direct reader

--- lucene/core/src/java/org/apache/lucene/util/packed/DirectReader.java
+++ lucene/core/src/java/org/apache/lucene/util/packed/DirectReader.java
@@ -48,7 +48,113 @@ public class DirectReader {
    * value
    */
   public static LongValues getInstance(RandomAccessInput slice, int bitsPerValue) {
-    return getInstance(slice, bitsPerValue, 0);
+    return switch (bitsPerValue) {
+      case 1 -> index -> {
+        try {
+          int shift = (int) (index & 7);
+          return slice.readByte(index >>> 3) >>> shift & 0x1;
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      };
+      case 2 -> index -> {
+        try {
+          int shift = (int) (index & 3) << 1;
+          return slice.readByte(index >>> 2) >>> shift & 0x3;
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      };
+      case 4 -> index -> {
+        try {
+          int shift = (int) (index & 1) << 2;
+          return slice.readByte(index >>> 1) >>> shift & 0xF;
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      };
+      case 8 -> index -> {
+        try {
+          return slice.readByte(index) & 0xFF;
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      };
+      case 12 -> index -> {
+        try {
+          int shift = (int) (index & 1) << 2;
+          return slice.readShort(index * 12 >>> 3) >>> shift & 0xFFF;
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      };
+      case 16 -> index -> {
+        try {
+          return slice.readShort(index << 1) & 0xFFFF;
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      };
+      case 20 -> index -> {
+        try {
+          int shift = (int) (index & 1) << 2;
+          return slice.readInt(index * 20 >>> 3) >>> shift & 0xFFFFF;
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      };
+      case 24 -> index -> {
+        try {
+          return slice.readInt(index * 3) & 0xFFFFFF;
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      };
+      case 28 -> index -> {
+        try {
+          int shift = (int) (index & 1) << 2;
+          return slice.readInt(index * 28 >>> 3) >>> shift & 0xFFFFFFF;
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      };
+      case 32 -> index -> {
+        try {
+          return slice.readInt(index << 2) & 0xFFFFFFFFL;
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      };
+      case 40 -> index -> {
+        try {
+          return slice.readLong(index * 5) & 0xFFFFFFFFFFL;
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      };
+      case 48 -> index -> {
+        try {
+          return slice.readLong(index * 6) & 0xFFFFFFFFFFFFL;
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      };
+      case 56 -> index -> {
+        try {
+          return slice.readLong(index * 7) & 0xFFFFFFFFFFFFFFL;
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      };
+      case 64 -> index -> {
+        try {
+          return slice.readLong(index << 3);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      };
+      default -> throw new IllegalArgumentException("unsupported bitsPerValue: " + bitsPerValue);
+    };
   }
 
   /**
@@ -60,15 +166,15 @@ public class DirectReader {
       case 1 -> index -> {
         try {
           int shift = (int) (index & 7);
-          return (slice.readByte(offset + (index >>> 3)) >>> shift) & 0x1;
+          return slice.readByte(offset + (index >>> 3)) >>> shift & 0x1;
         } catch (IOException e) {
           throw new RuntimeException(e);
         }
       };
       case 2 -> index -> {
         try {
-          int shift = ((int) (index & 3)) << 1;
-          return (slice.readByte(offset + (index >>> 2)) >>> shift) & 0x3;
+          int shift = (int) (index & 3) << 1;
+          return slice.readByte(offset + (index >>> 2)) >>> shift & 0x3;
         } catch (IOException e) {
           throw new RuntimeException(e);
         }
@@ -76,7 +182,7 @@ public class DirectReader {
       case 4 -> index -> {
         try {
           int shift = (int) (index & 1) << 2;
-          return (slice.readByte(offset + (index >>> 1)) >>> shift) & 0xF;
+          return slice.readByte(offset + (index >>> 1)) >>> shift & 0xF;
         } catch (IOException e) {
           throw new RuntimeException(e);
         }
@@ -91,7 +197,7 @@ public class DirectReader {
       case 12 -> index -> {
         try {
           int shift = (int) (index & 1) << 2;
-          return (slice.readShort(offset + ((index * 12) >>> 3)) >>> shift) & 0xFFF;
+          return slice.readShort(offset + (index * 12 >>> 3)) >>> shift & 0xFFF;
         } catch (IOException e) {
           throw new RuntimeException(e);
         }
@@ -106,7 +212,7 @@ public class DirectReader {
       case 20 -> index -> {
         try {
           int shift = (int) (index & 1) << 2;
-          return (slice.readInt(offset + ((index * 20) >>> 3)) >>> shift) & 0xFFFFF;
+          return slice.readInt(offset + (index * 20 >>> 3)) >>> shift & 0xFFFFF;
         } catch (IOException e) {
           throw new RuntimeException(e);
         }
@@ -121,7 +227,7 @@ public class DirectReader {
       case 28 -> index -> {
         try {
           int shift = (int) (index & 1) << 2;
-          return (slice.readInt(offset + ((index * 28) >>> 3)) >>> shift) & 0xFFFFFFF;
+          return slice.readInt(offset + (index * 28 >>> 3)) >>> shift & 0xFFFFFFF;
         } catch (IOException e) {
           throw new RuntimeException(e);
         }
@@ -213,7 +319,7 @@ public class DirectReader {
           // bitsPerValue is a multiple of 8: 8, 16, 24, 32, 30, 48, 56, 64
           final int bytesPerValue = bitsPerValue / Byte.SIZE;
           final long mask = bitsPerValue == 64 ? ~0L : (1L << bitsPerValue) - 1;
-          long offset = baseOffset + (index * bitsPerValue) / 8;
+          long offset = baseOffset + index * bitsPerValue / 8;
           for (int i = 0; i < MERGE_BUFFER_SIZE; ++i) {
             if (bitsPerValue > Integer.SIZE) {
               buffer[i] = slice.readLong(offset) & mask;
@@ -230,12 +336,12 @@ public class DirectReader {
           // bitsPerValue is 1, 2 or 4
           final int valuesPerLong = Long.SIZE / bitsPerValue;
           final long mask = (1L << bitsPerValue) - 1;
-          long offset = baseOffset + (index * bitsPerValue) / 8;
+          long offset = baseOffset + index * bitsPerValue / 8;
           int i = 0;
           for (int l = 0; l < 2 * bitsPerValue; ++l) {
             final long bits = slice.readLong(offset);
             for (int j = 0; j < valuesPerLong; ++j) {
-              buffer[i++] = (bits >>> (j * bitsPerValue)) & mask;
+              buffer[i++] = bits >>> j * bitsPerValue & mask;
             }
             offset += Long.BYTES;
           }
@@ -244,7 +350,7 @@ public class DirectReader {
           // Read values 2 by 2
           final int numBytesFor2Values = bitsPerValue * 2 / Byte.SIZE;
           final long mask = (1L << bitsPerValue) - 1;
-          long offset = baseOffset + (index * bitsPerValue) / 8;
+          long offset = baseOffset + index * bitsPerValue / 8;
           for (int i = 0; i < MERGE_BUFFER_SIZE; i += 2) {
             final long l;
             if (numBytesFor2Values > Integer.BYTES) {
@@ -253,7 +359,7 @@ public class DirectReader {
               l = slice.readInt(offset);
             }
             buffer[i] = l & mask;
-            buffer[i + 1] = (l >>> bitsPerValue) & mask;
+            buffer[i + 1] = l >>> bitsPerValue & mask;
             offset += numBytesFor2Values;
           }
         }
